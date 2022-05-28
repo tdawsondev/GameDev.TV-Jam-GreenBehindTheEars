b73f40fe70be4e8ac985abb3455944c62e040bff
Robert Heslin <robert.heslin.2@gmail.com>
2022-05-28 10:52:13 -0400

    Added House Levels for the player, winter, and gravekeeper. These are all bare bones and empty except for a door which takes you back to the overworld scene. This does not drop you outside of the door, but back on the docks currently.

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -57,6 +57,8 @@ public class Player : MonoBehaviour
 
     public void OnPause()
     {
+        Debug.Log($"Trying to pause.");
+
         if (settings == null) { return; }
         settings.SetPauseState(true);
 
--- Assets/Scripts/Settings/SceneHandler.cs
+++ Assets/Scripts/Settings/SceneHandler.cs
@@ -13,9 +13,10 @@ public class SceneHandler : MonoBehaviour
     {
         title = 0,
         ghostTownOverworld = 1,
-        kabungusTest = 2,
-        dreamsTest = 3,
-        BoatScene = 4,
+        BoatScene = 2,
+        PlayerHouse = 3,
+        WinterHouse = 4,
+        GravekeeperHouse = 5
     }
 
     private void Start()
@@ -24,36 +25,12 @@ public class SceneHandler : MonoBehaviour
     }
 
     public void LoadLevel(LEVELS levelName)
-    {
-        int buildIndex = -1;
-        switch (levelName)
-        {
-            case LEVELS.title:
-                SaveSystem.DeleteAllTemps(); // All Temps deleted when going to main menu
-                buildIndex = 0;
-                break;
-            case LEVELS.ghostTownOverworld:
-                buildIndex = 1;
-                break;
-            case LEVELS.kabungusTest:
-                settings.SetMusic(AudioHandler.AUDIO_MUSIC.town);
-                buildIndex = 2;
-                break;
-            case LEVELS.dreamsTest:
-                buildIndex = 3;
-                break;
-            default:
-                break;
-        }
-
-        //if(buildIndex == -1) { return; }
-        
+    { 
         SceneManager.LoadScene((int)levelName);
     }
 
     public void RestartLevel()
     {
-
         Scene curScene = SceneManager.GetActiveScene();
         SceneManager.LoadScene(curScene.buildIndex);
     }
