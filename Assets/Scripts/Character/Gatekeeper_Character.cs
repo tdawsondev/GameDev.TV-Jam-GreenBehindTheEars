ac8f9d590dd579735cd1b366b5aacc99987298e7
tdawson <thomasdawsondev@gmail.com>
2022-05-30 13:06:15 -0500

    FInished park state

--- Assets/Scripts/Character/Gatekeeper_Character.cs
+++ Assets/Scripts/Character/Gatekeeper_Character.cs
@@ -8,7 +8,11 @@ public class Gatekeeper_Character : Character
     public DialogObject TakeASeat;
     public DialogObject Follow_Instructions;
     public DialogObject afterHouse;
-    public DialogObject parkConvo;
+    public DialogObject parkEntry;
+    public DialogObject parkBeforeHat;
+    public DialogObject parkRecurring;
+
+
 
     [Header("Destinations")]
     public Transform parkLocation;
